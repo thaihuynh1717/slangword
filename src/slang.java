56b97bb31b26f60acb65af817a317d6ac8e5762a
thaihuynh1717 <thaihuynh1717@gmail.com>
2022-07-09 12:10:54 +0700

    3rd commit

---
+++ src/slang.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class slang {
+    private ArrayList<String> slangWord = new ArrayList<String>();
+    private ArrayList<ArrayList<String>> definition = new ArrayList<ArrayList<String>>();
+
+}
